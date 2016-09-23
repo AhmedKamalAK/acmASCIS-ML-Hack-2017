18cba9c0c9e60ac6052edabac688bfebeddfc6fa
AHMEDKAMAL-LAP\ahmed <ahmed.kamal@cis.asu.edu.eg>
2016-09-23 15:37:33 +0200

    Implemented submit module

---
+++ AssignmentsSubmission/main.py
@@ -0,0 +1,8 @@
+from submit import submit_assignment
+
+
+def main():
+    submit_assignment(['a', 'str', 1], '1O4nOl-UvVjGvuM_zrES3vvPcV4HEkzAl9PuuKen36uQ', 'TestSheet')
+
+if __name__ == '__main__':
+    main()
