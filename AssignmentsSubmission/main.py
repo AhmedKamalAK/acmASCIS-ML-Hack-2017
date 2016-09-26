afa60e990a1e6931258206e01c17f90e84937ffc
AhmedKamalAK <ahmed.kamal@cis.asu.edu.eg>
2016-09-26 20:05:48 +0200

    Refactored getting credentials and service part

--- AssignmentsSubmission/main.py
+++ AssignmentsSubmission/main.py
@@ -1,8 +1,10 @@
 from submit import submit_assignment
+from sheets_service import get_sheets_service
 
 
 def main():
-    submit_assignment(['a', 'str', 1], '1O4nOl-UvVjGvuM_zrES3vvPcV4HEkzAl9PuuKen36uQ', 'TestSheet')
+    service = get_sheets_service()
+    submit_assignment(['a', 'str', 1], '1O4nOl-UvVjGvuM_zrES3vvPcV4HEkzAl9PuuKen36uQ', 'TestSheet', service)
 
 if __name__ == '__main__':
     main()
